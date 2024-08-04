28f5b02ac2f0d6b6069dbfac4e61c4593bde3e09
ahmed hossam <ahmdhsammfarge@gmail.com>
2024-08-04 13:03:05 +0300

    elf #

--- kernel/source/process/elf.c
+++ kernel/source/process/elf.c
@@ -1,5 +1,6 @@
 #include "elf.h"
 #include "libc.h"
+#include "info.h"
 
 /* Check the file is elf */
 static inline uint8_t elf_check_file(elf32_ehdr_t *hdr) {
@@ -30,26 +31,39 @@ static inline uint8_t elf_check_executable(elf32_ehdr_t *hdr) {
     return hdr->e_type == ET_EXEC;
 }
 
+static inline uint8_t elf_check_mregion(uint32_t offset, uint32_t size){
+    #define between(x, a, b) ((x) >= (a) && (x) <= (b))
+    #define intersect(x,y,a,b) (between(x, a, b) || between(y, a, b) || between(a, x, y)) 
+    if(intersect(offset, offset+size, KERNEL_OFFSET, KERNEL_END-1)) return 0;
+    if(intersect(offset, offset+size, DRIVERS_OFFSET, 0xFFFFFFFF)) return 0;
+    return 1;
+}
+
 /* Get program header start */
 static inline elf32_phdr_t *elf_pheader(elf32_ehdr_t *hdr) {
 	return (elf32_phdr_t *)((uint32_t)hdr + hdr->e_phoff);
 }
 
 /* Load program segment into memory*/
-static inline void elf_load_psegment(elf32_phdr_t *phdr, char* file) {
+static inline uint8_t elf_load_psegment(elf32_phdr_t *phdr, char* file) {
+    if(!elf_check_mregion(phdr->p_vaddr, phdr->p_memsz)) return 0;
+
     char* sagment = mem_set((char*)phdr->p_vaddr, 0, phdr->p_memsz);
     mem_copy((char*)phdr->p_vaddr, file + phdr->p_offset, phdr->p_filesz);
+
+    return 1;
 }
 
 /* Read all program headers */
-static inline void elf_read_program_headers(elf32_ehdr_t *ehdr) {
+static inline uint8_t elf_read_program_headers(elf32_ehdr_t *ehdr) {
     elf32_phdr_t* phdr = elf_pheader(ehdr);
     for (int i = 0; i < ehdr->e_phnum; i++) {
         if (phdr->p_type == PT_LOAD) {
-            elf_load_psegment(phdr, (char*)ehdr);
+            if(!elf_load_psegment(phdr, (char*)ehdr)) return 0;
         }
         phdr ++;
     }
+    return 1;
 }
 
 uint8_t elf_load_file(void* file, uint32_t* eip){
@@ -57,7 +71,7 @@ uint8_t elf_load_file(void* file, uint32_t* eip){
     if(!elf_check_file(ehdr) || !elf_check_supported(ehdr) || !elf_check_executable(ehdr)){
         return 0;
     }
-    elf_read_program_headers(ehdr);
+    if(!elf_read_program_headers(ehdr)) return 0;
     *eip = ehdr->e_entry;
     return 1;
 }
